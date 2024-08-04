d30042f3cdad22000c8fa16864948edbc8137718
Guilhermehstrabello <gui.strabehonorio@outlook.com>
2024-08-04 19:14:42 -0300

    atualização de domingo, quase finalizada

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        lg: "1024px",
+      },
+    },
+  },
+  plugins: [],
+};
